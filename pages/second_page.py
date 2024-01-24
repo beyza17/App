f439a96d6037b29d8b86d9c3b1d34643ffc96650
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:28:40 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -3,8 +3,21 @@ import streamlit as st
 
 def main():
     # open colour image
-    st.title('Top hat Transformation')
+    st.title('Model Output')
     st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
+    st.sidebar.title('Transformation')
+    col1, col2 = st.columns(2)
+    image_file = st.sidebar.file_uploader("Upload the image", type=['jpg', 'png', 'jpeg'])
+    if image_file is not None:
+        img = plt.imread(image_file)
+        col1.image(image_file, caption='Uploaded Image', use_column_width=True)
+    col2:
+    text_input = st.text_input(
+        "Enter some text 👇",
+        label_visibility=st.session_state.visibility,
+        disabled=st.session_state.disabled,
+        placeholder=st.session_state.placeholder,
+    )
    
 
 
