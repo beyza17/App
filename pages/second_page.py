0c15d7ecfb19c939145147f1ce75a6d70d898693
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:47:38 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -6,12 +6,14 @@ def main():
     st.title('Model Output')
     st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
     
-    if "visibility" not in st.session_state:
+    # Store the initial value of widgets in session state
+if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
 
 col1, col2 = st.columns(2)
-    with col1:
+
+with col1:
     st.checkbox("Disable text input widget", key="disabled")
     st.radio(
         "Set text input label visibility 👉",
@@ -23,16 +25,17 @@ col1, col2 = st.columns(2)
         "This is a placeholder",
         key="placeholder",
     )
-                
-    with col2:
+
+with col2:
     text_input = st.text_input(
         "Enter some text 👇",
         label_visibility=st.session_state.visibility,
         disabled=st.session_state.disabled,
         placeholder=st.session_state.placeholder,
     )
-        if text_input:
-            st.write("You entered: ", text_input)
+
+    if text_input:
+        st.write("You entered: ", text_input)
    
 
 
