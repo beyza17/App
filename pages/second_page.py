7ee51ae0e80dd57ea0573b90e40ff44c389813fb
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:44:25 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -8,7 +8,17 @@ def main():
     
     col1, col2 = st.columns(2)
     with col1:
-    st.markdown("[![Foo](https://media.giphy.com/media/1YBjlPx4dmZ2uSqYiH/giphy.gif)")
+    st.checkbox("Disable text input widget", key="disabled")
+    st.radio(
+        "Set text input label visibility 👉",
+        key="visibility",
+        options=["visible", "hidden", "collapsed"],
+    )
+    st.text_input(
+        "Placeholder for the other text input widget",
+        "This is a placeholder",
+        key="placeholder",
+    )
                 
     with col2:
     text_input = st.text_input(
