c1e8f2690a3e5a543f9a168cef532960f2c8ec8f
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 20:22:40 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -1,4 +1,6 @@
 import streamlit as st
+from streamlit_option_menu import option_menu   
+from streamlit_extras.colored_header import colored_header
 
 def main():
     ##st.markdown("<h2 style='text-align: center; color: blue;'>Smart Chest-Xray Analysis and Report Generation!</h2>", unsafe_allow_html=True)
