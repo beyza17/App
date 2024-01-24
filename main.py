00580a212e0fc6aaf3a8828475e808fc1a343902
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 19:50:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,12 +14,11 @@ if __name__=="__main__":
             """,
             unsafe_allow_html=True
         )
-    st.title("MedIQ ChatBot")
+    st.title(":blue[MedIQ ChatBot]")
     st.subheader("Your Virtual Doctor's Appointment")
 
     # Introduction and description
     st.markdown(
-        "<h1 style=color: blue;'>Title</h1>"
         """
         Welcome to the MedIQ ChatBot interface, a smart and intuitive platform designed to provide you with medical information and assistance in a convenient and personalized manner. MedIQ chatbot leverages advanced natural language processing and deep learning techniques to simulate a virtual doctor's appointment, addressing your medical queries and concerns.
         """
