451350e1afdedbe70b0ab34e874fbe5181fb1cd3
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:20:49 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 
 def main():
+    # open colour image
+    st.title('Top hat Transformation')
     st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
    
 
