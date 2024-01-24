8bed3e0870c37f5a99110517db01d0bec640085c
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 20:10:07 +0100

    Update Second.py

--- Pages/Second.py
+++ Pages/Second.py
@@ -29,4 +29,9 @@ def display_chat(chat_messages):
                 f'</div>',
                 unsafe_allow_html=True
             )
+# Run the chat interface
+if __name__ == '__main__':
+    # Set Streamlit app title
+    st.title("MedIQ ChatBot")
 
+    chat_interface(st.session_state["chat_messages"])
