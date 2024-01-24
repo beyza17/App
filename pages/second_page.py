861ba9498dce6344db24e34a9ee5eb43f3bb0c66
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 22:38:47 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -18,14 +18,14 @@ def main():
     
     with col2:
         text_input = st.text_input(
-        "Enter some text 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
+            "Enter some text 👇",
+            label_visibility=st.session_state.visibility,
+            disabled=st.session_state.disabled,
+            placeholder=st.session_state.placeholder,
+        )
 
-    if text_input:
-        st.write("You entered: ", text_input)
+        if text_input:
+            st.write("You entered: ", text_input)
         
 
 if __name__ == "__main__":
