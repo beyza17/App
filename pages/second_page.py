89f77217775ba9d7460519010d8385ba8c31f669
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 22:40:13 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -6,10 +6,7 @@ def main():
     st.title('Model Output')
     st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
     
-       # Store the initial value of widgets in session state
-    if "visibility" not in st.session_state:
-        st.session_state.visibility = "visible"
-        st.session_state.disabled = False
+       
     
     col1, col2 = st.columns(2)
     
@@ -17,12 +14,7 @@ def main():
         st.markdown("[![Foo](https://media.giphy.com/media/RqxdeXRrOiGic/giphy.gif)")
     
     with col2:
-        text_input = st.text_input(
-            "Enter some text 👇",
-            label_visibility=st.session_state.visibility,
-            disabled=st.session_state.disabled,
-            placeholder=st.session_state.placeholder,
-        )
+        text_input = st.text_input("Enter some text 👇")
 
         if text_input:
             st.write("You entered: ", text_input)
