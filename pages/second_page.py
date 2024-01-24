7fe96f39089ce202a85099838e32fb672bdb2f55
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:50:42 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -2,28 +2,28 @@ import streamlit as st
 
 
 def main():
-# open colour image
-st.title('Model Output')
-st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
+    # open colour image
+    st.title('Model Output')
+    st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
     
-# Store the initial value of widgets in session state
+    # Store the initial value of widgets in session state
 
 
-col1, col2 = st.columns(2)
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.markdown("[![Foo](https://media.giphy.com/media/RqxdeXRrOiGic/giphy.gif)")
+    with col1:
+        st.markdown("[![Foo](https://media.giphy.com/media/RqxdeXRrOiGic/giphy.gif)")
 
-with col2:
-    text_input = st.text_input(
-        "Enter some text 👇",
-        label_visibility=st.session_state.visibility,
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
+    with col2:
+        text_input = st.text_input(
+            "Enter some text 👇",
+            label_visibility=st.session_state.visibility,
+            disabled=st.session_state.disabled,
+            placeholder=st.session_state.placeholder,
+        )
 
-    if text_input:
-        st.write("You entered: ", text_input)
+        if text_input:
+            st.write("You entered: ", text_input)
    
 
 
