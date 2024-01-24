c55ed963aef26ca3000e5e42337012ee5df42e6f
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 21:34:03 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -7,7 +7,7 @@ def main():
     st.markdown("<h2 style='text-align: center; color: violet;'>Report Generation!</h2>", unsafe_allow_html=True)
     
     col1, col2 = st.columns(2)
-    image_file = st.sidebar.file_uploader("Upload the image", type=['jpg', 'png', 'jpeg'])
+    image_file = st.file_uploader("Upload the image", type=['jpg', 'png', 'jpeg'])
     if image_file is not None:
         img = plt.imread(image_file)
         col1.image(image_file, caption='Uploaded Image', use_column_width=True)
