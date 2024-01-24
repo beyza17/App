5b2517dfcb0fa3a9f0d9b10d7fb8c1c79420e510
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-24 20:13:53 +0100

    Update second.py

--- pages/second.py
+++ pages/second.py
@@ -1,1 +1,33 @@
+import streamlit as st
+
+def display_chat(chat_messages):
+    # Display chat messages
+    for sender, message in chat_messages:
+        a = ":smiley:"
+        if sender == 'user':
+            # Apply CSS styling for user response
+            st.write(
+                f'<div style="display:flex; align-items:center; flex-direction:row-reverse;">'
+                f'<div style="margin-right: 10px;">'
+                f'👤'  # User emoji
+                f'</div>'
+                f'<div style="background-color:#25D366; padding:10px; border-radius:10px;">'
+                f'{message}'
+                f'</div>'
+                f'</div>',
+                unsafe_allow_html=True
+            )
+        else:
+            # Apply CSS styling for bot response
+            st.write(
+                f'<div style="display:flex; align-items:center;">'
+                f'<div style="margin-right: 10px;">'
+                f'🤖'  # Bot emoji
+                f'</div>'
+                f'<div style="background-color:#075E54; padding:10px; border-radius:10px;">'
+                f'{message}'
+                f'</div>'
+                f'</div>',
+                unsafe_allow_html=True
+            )
 
