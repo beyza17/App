3038a6ee792238136dbe75d78c1f366e6e7fe541
Beyza Zayim <117541981+beyza17@users.noreply.github.com>
2024-01-25 19:46:55 +0100

    Update second_page.py

--- pages/second_page.py
+++ pages/second_page.py
@@ -17,6 +17,26 @@ def main():
 
         if text_input:
             st.write("You entered: ", text_input)
+                    
+
+       ########
+            nlp = spacy.load('en_core_web_sm')
+
+            # Create a Doc object
+            doc = nlp(text_input)
+            # Generate list of lemmas
+            lemmas = [token.lemma_ for token in doc]
+            st.write("You entered: ", lemmas)
+
+            # Remove tokens that are not alphabetic
+            a_lemmas = [lemma for lemma in lemmas
+            if lemma.isalpha() or lemma == '-PRON-']
+            # Print string after text cleaning
+            st.write("You entered: ", a_lemmas)
+            st.write("You entered: ", (' '.join(a_lemmas)))
+            # Generate list of tokens and pos tags
+            pos = [(token.text, token.pos_) for token in doc]
+            st.write("You entered: ", pos)
             
 
 if __name__ == "__main__":
